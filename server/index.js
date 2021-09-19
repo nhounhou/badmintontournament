f692d7356047bbb0f8c06e3892d8644ac5c1fb06
unknown <nhounhou@hotmail.com>
2021-09-19 13:31:49 -0400

    adding ENV variables

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import postRoutes from './routes/posts.js';
 
@@ -14,10 +16,9 @@ app.use(cors());
 
 app.use('/players', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://michel:mernstack@cluster0.oftj5.mongodb.net/tournament?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
